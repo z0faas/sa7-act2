a7e376e69e193d83a799c96e387fb8c471f6f026
Jonathan Maronde <j2mike4@gmail.com>
2024-02-17 15:05:40 -0600

    Completed prob 14, using map to create a new array with the length of each string

---
+++ prob_14.rb
@@ -0,0 +1,6 @@
+'''
+Using map to create a new array with the length of each string
+'''
+
+strings = ['Jonathan', 'Dr Pepper', 'CS3', 'Supercalifragilisticexpialidocious']
+p strings.map { |string| string.length }

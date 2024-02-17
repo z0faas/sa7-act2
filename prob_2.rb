47c7255ec7594a6ed42f3ef774f6152358ea6908
Jonathan Maronde <j2mike4@gmail.com>
2024-02-17 12:22:51 -0600

    Completed prob 2, doubling each # in an arry then tripling the numbers with the map method

---
+++ prob_2.rb
@@ -0,0 +1,10 @@
+'''
+Printing each number in an array doubled, then created a new array with the original numbers tripled
+'''
+
+numbers = [1, 2, 3, 4, 5]
+numbers.each do |number|
+  puts number * 2
+end
+
+puts numbers.map { |n| n * 3 }
